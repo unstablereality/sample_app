9cb60c5df2d391eda4af2d0e403a685ea2ceb8df
unstablereality <unstableraelity@gmail.com>
2013-10-29 15:28:40 -0500

    Finish CHapter 7 Exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,31 @@ describe "User pages" do
 			it "should not create a user" do
 				expect { click_button submit }.not_to change(User, :count)
 			end
+			
+			describe "after submission with no data" do
+				before { click_button submit }
+				
+				it { should have_title('Sign up') }
+				it { should have_content('error') }
+				it { should have_content('Name can\'t be blank') }
+				it { should have_content('Email can\'t be blank') }
+				it { should have_content('Email is invalid') }
+				it { should have_content('Password can\'t be blank') }
+				it { should have_content('Password is too short (minimum is 6 characters)') }
+			end
+			
+			describe "after submission with invalid password confirmation" do
+				before do
+					fill_in "Name",			with: "Example User"
+					fill_in "Email", 		with: "user@example.com"
+					fill_in "Password", with: "foobar"
+					
+					click_button submit
+				end
+				
+				it { should have_content('Password confirmation doesn\'t match Password') }
+				it { should have_content('Password confirmation can\'t be blank') }
+			end
 		end
 		
 		describe "with valid information" do
@@ -41,6 +66,14 @@ describe "User pages" do
 			
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
+			end
+			
+			describe "after saving the user" do
+				before { click_button submit }
+				let(:user) { User.find_by(email: 'user@example.com' ) }
+				
+				it { should have_title(user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
 			end
 		end
 	end
